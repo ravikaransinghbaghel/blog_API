f3c5a1743a57d67a8a69b60622396d63d4c50b52
ravikaransinghbaghel <ravikaransingh124@gami.com>
2025-06-17 17:42:56 +0530

    use env variable for frontend

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-    origin:'https://rk-blog-nine.vercel.app',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
 }))
 app.use(express.json())
