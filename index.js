680650821ac9f018d5411069b36a06e7782f7934
ravikaransinghbaghel <ravikaransingh124@gami.com>
2025-06-18 13:11:25 +0530

    replace to old commit

--- index.js
+++ index.js
@@ -13,22 +13,19 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({
-    origin:'https://rk-blog-nine.vercel.app/',
-    credentials: true,
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use('/api', router);
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'))
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'public/sory.html'))
-});
+// app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/sory.html'))
+// });
 
 
 const port = process.env.port || 3000;
